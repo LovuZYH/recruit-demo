02facd08920c616c30d80634386e49b5e3304da2
LovuZYH <386091128@qq.com>
2019-09-09 21:45:27 +0800

    第一次上传 recruit-interface

---
+++ recruit-dao/src/main/java/com/zp/mapper/RecruitMapper.java
@@ -0,0 +1,11 @@
+package com.zp.mapper;
+
+/**
+ * @author zyh
+ * @version 1.0.0
+ * @ClassName RecruitMapper.java
+ * @Description TODO
+ * @createTime 2019年09月09日 19:49:00
+ */
+public interface RecruitMapper {
+}

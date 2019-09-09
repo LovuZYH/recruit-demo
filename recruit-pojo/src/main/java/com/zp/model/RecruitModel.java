f4cec5d46a0dfe3633f2924163d91fdc22513214
LovuZYH <386091128@qq.com>
2019-09-09 21:34:22 +0800

    第一次上传 recruit-pojo

---
+++ recruit-pojo/src/main/java/com/zp/model/RecruitModel.java
@@ -0,0 +1,19 @@
+/**
+ * @ClassName RecruitModel.java
+ * @author zyh
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2019年09月09日 21:30:00
+ */
+package com.zp.model;
+
+/**
+ * @author zyh
+ * @version 1.0.0
+ * @ClassName RecruitModel.java
+ * @Description TODO
+ * @createTime 2019年09月09日 21:30:00
+ */
+public class RecruitModel {
+
+}

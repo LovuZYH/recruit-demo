0521f12e0dc20de0ae6ee3a65a653e03db103640
LovuZYH <386091128@qq.com>
2019-09-09 21:36:36 +0800

    第一次上传 recruit-interface

---
+++ recruit-interface/src/main/java/com/zp/service/RecruitService.java
@@ -0,0 +1,11 @@
+package com.zp.service;
+
+/**
+ * @author zyh
+ * @version 1.0.0
+ * @ClassName RecruitService.java
+ * @Description TODO
+ * @createTime 2019年09月09日 19:49:00
+ */
+public interface RecruitService {
+}
